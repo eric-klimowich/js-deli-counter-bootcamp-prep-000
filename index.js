43162be9f64a2a5f9fe665133b2ad45134df4fad
Eric Klimowich <eklimowich@hotmail.com>
2018-08-10 04:25:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function currentLine(katzDeliLine) {
   if (katzDeliLine === 0) {
     return 'The line is currently empty.'
   }
-  var order = `The line is currently: 1. `;
+  var order = `The line is currently: `;
   for (var i = 0; i < katzDeliLine.length; i++) {
-    order += `, ${i}. ${katzDeliLine[i]}`
+    order += `, ${i + 1}. ${katzDeliLine[i]}`
   }
   return order;
 }
