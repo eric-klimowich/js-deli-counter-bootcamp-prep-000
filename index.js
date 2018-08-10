8543c856ad12595438075f5ac34deb55ea730bb2
Eric Klimowich <eklimowich@hotmail.com>
2018-08-10 04:20:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ function currentLine(katzDeliLine) {
     return 'The line is currently empty.'
   }
   for (var i = 0; i < katzDeliLine.length; i++) {
-    return `The line is currently: ${i + 1}. ${katzDeliLine[i]}`;
+    return `${i + 1}. ${katzDeliLine[i]}`;
   }
+  return 
 }
