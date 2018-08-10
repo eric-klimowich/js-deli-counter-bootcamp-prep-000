c7a5eb1190671e144b3187fb9699efb2dfab502e
Eric Klimowich <eklimowich@hotmail.com>
2018-08-10 04:03:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function takeANumber(katzDeliLine, newPerson) {
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
-    return 'The line is currently empty.';
+    return 'There is nobody waiting to be served!';
   }
   
 }
